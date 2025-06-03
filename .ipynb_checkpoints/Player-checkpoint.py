5c0d9d1eff9c353cc0481d973946c233819a7afa
FelixBerget <felixberget@gmail.com>
2025-06-03 16:08:27 +0200

    setting up movement and model

--- .ipynb_checkpoints/Player-checkpoint.py
+++ .ipynb_checkpoints/Player-checkpoint.py
@@ -2,6 +2,8 @@ from pynput.keyboard import Key
 from pynput.keyboard import Controller as KeyboardController
 import time
 
+
+#Testing command inputs
 keyboard = KeyboardController()
 time.sleep(3)
 
@@ -19,9 +21,37 @@ time.sleep(2)
 keyboard.press('n')
 keyboard.release('n')
 
+keepTrying = True
+
 def test():
     time.sleep(2)
     keyboard.type(" Flats")
     keyboard.press(Key.left)
     keyboard.release(Key.left)
 test()
+
+#Commands to control the inputs
+def turnLeft():
+    keyboard.press(Key.left)
+    keyboard.release(Key.left)
+
+def turnRight():
+    keyboard.press(Key.right)
+    keyboard.release(Key.rigth)
+
+def turnDown():
+    keyboard.press(Key.down)
+    keyboard.release(Key.down)
+
+def turnUp():
+    keyboard.press(Key.up)
+    keyboard.release(Key.up)
+
+
+
+#Test for playing constantly
+while keepTrying:
+    time.sleep(3)
+    print("Player Player")
+    
+
