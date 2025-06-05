f2e616c3574427664dad579bedee12167ea5012c
FelixBerget <felixberget@gmail.com>
2025-06-05 14:39:07 +0200

    startet with reading images

--- Player.py
+++ Player.py
@@ -4,8 +4,12 @@ import time
 from PIL import Image, ImageGrab
 from tkinter import *
 from tkinter.ttk import *
+from pytesseract import pytesseract
 
-
+#This is what reads the score
+pathTesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#The Reward that will be given based on all the choices the model has made
+currentReward = 0
 #Gets all the info about the screen
 screenInfo = Tk()
 screenHeight = screenInfo.winfo_screenheight()
@@ -15,6 +19,7 @@ keepTrying = True
 currnetDecision = []
 #Testing command inputs
 keyboard = KeyboardController()
+pytesseract.tesseract_cmd = pathTesseract
 
 #Commands to control the inputs
 def turnLeft():
@@ -26,8 +31,8 @@ def turnRight():
     keyboard.release(Key.rigth)
 
 def turnDown():
-    keyboard.press(Key.down)
-    keyboard.release(Key.down)
+    keyboard.press(Key.space)
+    keyboard.release(Key.space)
 
 
 
@@ -37,6 +42,8 @@ while keepTrying:
     print(screenHeight)
     print(screenWidth)
     screenshot = ImageGrab.grab(bbox =(screenWidth/4,screenHeight/5,screenWidth/1.7,screenHeight)) 
+    screenshotText = pytesseract.image_to_string(screenshot)
+    print(screenshotText)
     screenshot.show()
     print("tatt screenshot")
     
